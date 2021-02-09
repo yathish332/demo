bd92781ab601b6578374e052659db6ee8a47c434
yathish332 <73523873+yathish332@users.noreply.github.com>
2021-02-09 18:14:27 +0530

    Add files via upload

---
+++ ign.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  9 18:14:02 2021
+
+@author: LENOVO
+"""
+
+
+venv/
+
+*.pyc
+__pycache__/
+
+instance/
+
+.pytest_cache/
+.coverage
+htmlcov/
+
+dist/
+build/
+*.egg-info/
+
+.DS_Store
